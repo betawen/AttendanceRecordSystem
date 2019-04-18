cb4ef51ea9622fcc90ef7a505e425d116edd7141
betawen <jing_find@163.com>
2019-04-18 19:25:46 +0800

    reset

---
+++ web/src/components/DataManage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "DataManage"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div id="home">
+    <div id="header">
+
+    </div>
+    <section id="container">
+      <router-view></router-view>
+    </section>
+    <div id="footer">
+      <p>华中科技大学Dian团队 16级种子班</p>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+  export default {
+    name: "Home"
+  }
+</script>
+
+<style scoped>
+
+</style>
