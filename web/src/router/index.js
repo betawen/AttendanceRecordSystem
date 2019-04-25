d16d5efaadcbe0378bdca08409aa777976181479
betawen <jing_find@163.com>
2019-04-25 17:32:39 +0800

    g2 helloworld

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Home from '../components/Home'
 import User from '../components/User'
 import Data from '../components/Data'
 import Search from '../components/Search'
 import Message from "../components/Message"
+import HelloWorld from '../components/HelloWorld'
 
 Vue.use(Router)
 
@@ -35,6 +35,10 @@ export default new Router({
     {
       path: '/message',
       component: Message
+    },
+    {
+      path: '/chart',
+      component: HelloWorld
     }
   ]
 })
